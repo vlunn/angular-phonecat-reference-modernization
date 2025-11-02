ff0738a622af889bb44671ef0bf725f9dac5c6ef
Vivian Lunnikivi <vivian.lunnikivi@futurice.com>
2025-11-02 21:46:06 +0200

    Turn component controllers to TypeScript classes.

--- app/phone-detail/phone-detail.component.ts
+++ app/phone-detail/phone-detail.component.ts
@@ -1,20 +1,24 @@
-'use strict';
+class PhoneDetailController {
+  phone: any;
+  mainImageUrl: string;
+
+  static $inject = ['$routeParams', 'Phone'];
+  constructor($routeParams: angular.route.IRouteParamsService, Phone: any) {
+    let phoneId = $routeParams.phoneId;
+    this.phone = Phone.get({phoneId}, (phone: any) => {
+      this.setImage(phone.images[0]);
+    });
+  }
+
+  setImage(imageUrl: string) {
+    this.mainImageUrl = imageUrl;
+  }
+}
 
 // Register `phoneDetail` component, along with its associated controller and template
 angular.
   module('phoneDetail').
   component('phoneDetail', {
     templateUrl: 'phone-detail/phone-detail.template.html',
-    controller: ['$routeParams', 'Phone',
-      function PhoneDetailController($routeParams, Phone) {
-        var self = this;
-        self.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone: any) {
-          self.setImage(phone.images[0]);
-        });
-
-        self.setImage = function setImage(imageUrl: string) {
-          self.mainImageUrl = imageUrl;
-        };
-      }
-    ]
+    controller: PhoneDetailController
   });
--- app/phone-list/phone-list.component.ts
+++ app/phone-list/phone-list.component.ts
@@ -1,14 +1,20 @@
-'use strict';
+class PhoneListController {
+  phones: any[];
+  orderProp: string;
+  query: string;
+
+  static $inject = ['Phone'];
+  constructor(Phone: any) {
+    this.phones = Phone.query();
+    this.orderProp = 'age';
+  }
+}
 
 // Register `phoneList` component, along with its associated controller and template
 angular.
   module('phoneList').
   component('phoneList', {
     templateUrl: 'phone-list/phone-list.template.html',
-    controller: ['Phone',
-      function PhoneListController(Phone) {
-        this.phones = Phone.query();
-        this.orderProp = 'age';
-      }
-    ]
+    controller: PhoneListController
   });
+
